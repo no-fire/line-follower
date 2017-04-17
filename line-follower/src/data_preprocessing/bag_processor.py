d7d25e10cfbe15554184b44837b184c4aaac2c96
laurengulland <lauren.gulland@students.olin.edu>
2017-04-17 16:30:51 -0400

    cleaned up code. comments, clarity, fixed an error.

--- line-follower/src/data_preprocessing/bag_processor.py
+++ line-follower/src/data_preprocessing/bag_processor.py
@@ -2,8 +2,8 @@
 
 '''Bag processor.
 	Inputs: - a directory name (e.g. 'run1'), which has a rosbag named source.bag in it.
-			- Optional second input: 'test' for if you want the program to stop after 20 images. Good for diagnostic runs.
-	SAMPLE RUN COMMAND: [run within line-follower package] `python src/data_preprocessing/bag_processor.py data/run1 test`
+			- Optional second input: 'test_run' for if you want the program to stop after 20 images, or 'test_img' if you want it to only run one image with no bag input. Good diagnostic runs.
+	SAMPLE RUN COMMAND: [run within line-follower package] `python src/data_preprocessing/bag_processor.py data/run1 test_run`
 	Outputs:    - many 640x480 .jpg images, named sequentially from 0000.jpg, in the respective run directory's "raw" folder
 				- cmd_vel.csv, which has two columns and each row corresponding to the images in order (from first line above). The columns are values for linear cmd_vel and angular cmd_vel.
 				- possibly a "small" folder with compressed images (similarly structured to the first line, above)
@@ -74,7 +74,7 @@ class BagProcessor(object):
 			if (topic=='/cmd_vel'):
 				#For each incoming cmd_vel, save the cmd_vel and the last image to their respective places.
 				self.latest_vel = np.matrix([msg.linear.x, msg.angular.z])
-				if (self.latest_img!=None):
+				if self.latest_img is not None: #if first cmd_vel comes before the first image, skip it.
 					self.add_vel()
 					self.latest_img_to_jpg()
 					self.index+=1
@@ -88,21 +88,17 @@ class BagProcessor(object):
 
 
 if __name__ == '__main__':
-	# dir_name = raw_input("Name of run directory in which to find source.bag:\n")
-	# if dir_name=='':
-	# 	dir_name='data/run1'
-
 	if len(sys.argv)>1:
 		dir_name = sys.argv[1]
 	else:
 		dir_name = 'data/sun_apr_16_office_full_line_1'
 	bp = BagProcessor(dir_name)
 
-	#is this a test?
-	if len(sys.argv)>2:
-		bp.test = bool(sys.argv[2]=='test' or sys.argv[2]=='true')
-	#Check to see the images end up in the right place with a unit test before the full run
-	# bp.test_image()
+	if len(sys.argv)>2: #check is this a test run based on parameters:
+		bp.test = bool(sys.argv[2]=='test_run')
 
-	bp.get_imgs()
-	np.savetxt(dir_name+'/cmd_vel.csv', bp.all_vel_array, delimiter=',') #save csv of all velocities
+	if len(sys.argv)>2 and sys.argv[2]=='test_img':
+		bp.test_image() 	#Check to see the images end up in the right place with a unit test before the full run
+	else: #run this normally.
+		bp.get_imgs()
+		np.savetxt(dir_name+'/cmd_vel.csv', bp.all_vel_array, delimiter=',') #save csv of all velocities
